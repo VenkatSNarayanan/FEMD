3fa2bef5b31361772a72280390351a1885176957
Vishwak Srinivasan <cs15btech11043@iith.ac.in>
2017-04-03 23:07:21 +0530

    Minor Changes and Floor Building

--- Dungeon.hs
+++ Dungeon.hs
@@ -1,5 +1,11 @@
 module Dungeon where
-    data Tile = Wall | Floor deriving(Show) -- Tile could be Wall or Floor
+    data Tile = Wall | Floor | Room -- Tile could be Wall or Floor
+    
+    instance Show Tile where        
+       show Wall = "[]"
+       show Floor = ".."
+       show Room = "$$"
+       
     data Direction = Up | Down | None deriving(Eq, Show) -- Direction could be Up or Down
     data RoomData = RoomData {up_left_x :: Int, up_left_y :: Int, down_right_x :: Int, down_right_y :: Int, x_centre :: Int, y_centre :: Int} -- Data type to store information about a given Room
 
@@ -9,7 +15,7 @@ module Dungeon where
     data PathData = PathData {start_x :: Int, start_y :: Int, end_x :: Int, end_y :: Int, bend_x :: Int, bend_y :: Int, bend_dir :: Direction} -- Data type to store information about a given Path between Rooms
     
     instance Show PathData where
-       show PathData {start_x = x1, start_y = y1, end_x = x2, end_y = y2, bend_x = xb, bend_y = yb, bend_dir = direct} = "(" ++ show x1 ++ "," ++ show y1 ++ ") to (" ++ show x2 ++ "," ++ show y2 ++ ")with bend " ++ show direct ++ "at (" ++ show xb ++ "," ++ show yb ++ ")\n"
+       show PathData {start_x = x1, start_y = y1, end_x = x2, end_y = y2, bend_x = xb, bend_y = yb, bend_dir = direct} = "(" ++ show x1 ++ "," ++ show y1 ++ ") to (" ++ show x2 ++ "," ++ show y2 ++ ") with bend " ++ show direct ++ " at (" ++ show xb ++ "," ++ show yb ++ ")\n"
 
     place_rooms :: Int -> Int -> Int -> [Int] -> ([Int], [RoomData]) -- Driver Function to build rooms for a given set of parameters
     place_rooms lngth brdth dnsty rndmSq = (place_rooms_impl (lngth, brdth, dnsty, rndmSq, []))
@@ -75,17 +81,17 @@ module Dungeon where
          
     make_path :: (RoomData, RoomData) -> PathData -- Function to build a path between 2 rooms
     make_path (room1, room2) 
-                           | (x_centre room1 == x_centre room2) = if y_centre room1 > y_centre room2 then
+                           | (x_centre room1 == x_centre room2) = if y_centre room1 < y_centre room2 then
                                                                      PathData {start_x = x_centre room1, start_y = down_right_y room1, end_x = x_centre room2, end_y = up_left_y room2, bend_x = x_centre room1, bend_y = down_right_y room1, bend_dir = Down}
                                                                   else 
                                                                      PathData {start_x = x_centre room1, start_y = up_left_y room1, end_x = x_centre room2, end_y = down_right_y room2, bend_x = x_centre room1, bend_y = up_left_y room1, bend_dir = Up}
                            | (y_centre room1 == y_centre room2) = PathData {start_x = down_right_x room1, start_y = y_centre room1, end_x = up_left_x room2, end_y = y_centre room2, bend_x = (0-1), bend_y = (0-1), bend_dir = None}
-                           | otherwise = if y_centre room2 < y_centre room1 then
+                           | otherwise = if y_centre room2 > y_centre room1 then
                                             PathData {start_x = down_right_x room1, start_y = y_centre room1, end_x = x_centre room2, end_y = down_right_y room2, bend_x = x_centre room2, bend_y = y_centre room1, bend_dir = Down}
                                          else 
                                             PathData {start_x = down_right_x room1, start_y = y_centre room1, end_x = x_centre room2, end_y = down_right_y room2, bend_x = x_centre room2, bend_y = y_centre room1, bend_dir = Up}
                                             
-    make_all_paths_1 :: [RoomData] -> [PathData]
+    make_all_paths :: [RoomData] -> [PathData]
     make_all_paths list_of_rooms = 
                            let 
                            sorted_rooms = sort_all_rooms list_of_rooms
@@ -95,3 +101,33 @@ module Dungeon where
     make_all_paths_impl :: [(RoomData, RoomData)] -> [PathData]
     make_all_paths_impl [] = []
     make_all_paths_impl zipped_rooms = make_path(head(zipped_rooms)) : make_all_paths_impl(tail(zipped_rooms))
+    
+    floor_row_init :: Int -> Tile -> [Tile]
+    floor_row_init 0 _ = []
+    floor_row_init lngth tiletype = tiletype : (floor_row_init (lngth - 1) tiletype)
+
+    floor_init :: Int -> Int -> Tile -> [[Tile]]
+    floor_init _ 0 _ = []
+    floor_init lngth brdth tiletype = (floor_row_init lngth tiletype) : (floor_init (lngth) (brdth - 1) tiletype)
+    
+    floor_row_mod :: Int -> Int -> [Tile] -> Tile -> [Tile]
+    floor_row_mod start end row new_tile = (take start row) ++ (floor_row_init (end - start + 1) new_tile) ++ (drop (end + 1) row)
+    
+    floor_rows_mod :: Int -> Int -> [[Tile]] -> Tile -> [[Tile]]
+    floor_rows_mod _ _ [] _ = []
+    floor_rows_mod start end cur_floor new_tile = (floor_row_mod start end (head(cur_floor)) new_tile) : floor_rows_mod start end (tail(cur_floor)) new_tile
+        
+    floor_rooms_mod :: [RoomData] -> [[Tile]] -> Tile -> [[Tile]]
+    floor_rooms_mod [] cur_floor _ = cur_floor
+    floor_rooms_mod my_rooms cur_floor new_tile = floor_rooms_mod (tail(my_rooms)) (floor_room_mod (head(my_rooms)) cur_floor new_tile) new_tile
+    
+    floor_room_mod :: RoomData -> [[Tile]] -> Tile -> [[Tile]]
+    floor_room_mod my_room cur_floor new_tile = let
+                                                leave_before = up_left_y my_room
+                                                leave_after = down_right_y my_room
+                                                (above_part, unchange_after) = splitAt (leave_after+1) cur_floor
+                                                (unchange_before, change_floor) = splitAt (leave_before) above_part
+                                                start_x = up_left_x my_room
+                                                end_x = down_right_x my_room
+                                                in
+                                                unchange_before ++ (floor_rows_mod start_x end_x change_floor new_tile) ++ unchange_after
