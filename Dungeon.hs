36cb99cd2d73c27751a505f63dd190ad3f352e3f
N Venkatasubrahmanian <venkat.darkwing@gmail.com>
2017-04-09 12:47:19 +0530

    Changed display system, and added player and monster notationally to the event loop.

--- Dungeon.hs
+++ Dungeon.hs
@@ -1,20 +1,21 @@
 module Dungeon where
     import Control.Monad
-    
+    import Data.List
+
     -- Tile can be one of Floor, Room and Path
     data Tile = Floor | Room | Path | Exit
-    
+
     -- How each tile should be displayed
-    instance Show Tile where        
-       show Floor = ".."
-       show Room = "$$"
-       show Path = "=="
+    instance Show Tile where
+       show Floor = " "
+       show Room = "."
+       show Path = "."
        show Exit = "XX"
-    
-    -- Directions. These are meant for traversing the Rooms (internally). Could be one of North, South, East or None   
+
+    -- Directions. These are meant for traversing the Rooms (internally). Could be one of North, South, East or None
     data Direction = North | South | East | None deriving(Eq, Show)
-    
-    -- RoomData type. All rooms are rectangular. This Data Type has the top left and bottom right coordinates with the centroid of the 
+
+    -- RoomData type. All rooms are rectangular. This Data Type has the top left and bottom right coordinates with the centroid of the
     -- Room
     data RoomData = RoomData {up_left_x :: Int, up_left_y :: Int, down_right_x :: Int, down_right_y :: Int, x_centre :: Int, y_centre :: Int}
 
@@ -26,7 +27,7 @@ module Dungeon where
     -- There might be bends in the path. These bends are right-angled
     -- The path is characterized by the starting location and direction, ending location and 1 bend if any.
     data PathData = PathData {start_x :: Int, start_y :: Int, start_dir :: Direction, end_x :: Int, end_y :: Int, bend_x :: Int, bend_y :: Int, bend_dir :: Direction}
-    
+
     -- Display each path (again for internal purposes)
     instance Show PathData where
        show PathData {start_x = x1, start_y = y1, start_dir = s_direct, end_x = x2, end_y = y2, bend_x = xb, bend_y = yb, bend_dir = b_direct} = "(" ++ show x1 ++ "," ++ show y1 ++ ") to (" ++ show x2 ++ "," ++ show y2 ++ ") with bend " ++ show b_direct ++ " at (" ++ show xb ++ "," ++ show yb ++ ") starting " ++ show s_direct ++ "\n"
@@ -76,10 +77,10 @@ module Dungeon where
     -- Implicit function to place a new room if possible based on current parameters in a list
     place_rooms_impl :: (Int, Int, Int, [Int], [RoomData]) -> ([Int], [RoomData])
     place_rooms_impl (lngth, brdth, dnsty, rndmSq, exist_rooms) =
-        if (dnsty == 0) then 
+        if (dnsty == 0) then
             (rndmSq, exist_rooms)
         else
-            let 
+            let
             genRand = take 4 rndmSq
             newrandSeq = drop 4 rndmSq
             num1 = head (genRand)
@@ -89,52 +90,52 @@ module Dungeon where
             genRoom = new_build_room num1 num2 num3 num4 lngth brdth
             roomCheck = foldr (&&) True (map (check_overlap genRoom) exist_rooms)
             in
-            if roomCheck then 
+            if roomCheck then
                place_rooms_impl (lngth, brdth, dnsty - 1, newrandSeq, (genRoom:exist_rooms))
             else place_rooms_impl (lngth, brdth, dnsty - 1, newrandSeq ,exist_rooms)
-            
+
     -- Function to sort all rooms by virtue of the absicca of the centroid of each room
     sort_all_rooms :: [RoomData] -> [RoomData]
     sort_all_rooms [] = []
     sort_all_rooms (x:xs) = (sort_all_rooms (left_part)) ++ (x : sort_all_rooms (right_part)) where
                     left_part = [room | room <- xs, (x_centre room) <= (x_centre x)]
                     right_part = [room | room <- xs, (x_centre room) > (x_centre x)]
-                                                   
+
 
     -- Function to build a path between any two rooms
     make_path :: (RoomData, RoomData) -> PathData
-    make_path (room1, room2) 
+    make_path (room1, room2)
                            | (x_centre(room1) == x_centre(room2)) = if (y_centre(room1) < y_centre(room2)) then
                                                                        PathData {start_x = x_centre(room1), start_y = down_right_y(room1), start_dir = East, end_x = x_centre(room2), end_y = up_left_y(room2), bend_x = x_centre(room1), bend_y = down_right_y(room1), bend_dir = South}
                                                                     else
                                                                        PathData {start_x = x_centre(room1), start_y = down_right_y(room1), start_dir = East, end_x = x_centre(room2), end_y = up_left_y(room2), bend_x = x_centre(room1), bend_y = down_right_y(room1), bend_dir = North}
 
                            | (y_centre(room1) == y_centre(room2)) = PathData {start_x = down_right_x(room1), start_y = y_centre(room1), start_dir = East, end_x = up_left_x(room2), end_y = y_centre(room2), bend_x = (0-1), bend_y = (0-1), bend_dir = None}
-                           
+
                            | (y_centre(room1) > y_centre(room2)) = if (down_right_y(room2) < y_centre(room1)) then
                                                                       PathData {start_x = down_right_x(room1), start_y = y_centre(room1), start_dir = East, end_x = x_centre(room2), end_y = down_right_y(room2), bend_x = x_centre(room2), bend_y = y_centre(room1), bend_dir = North}
 
-                                                                   else 
+                                                                   else
                                                                       if (up_left_y(room1) > y_centre(room2)) then
                                                                          PathData {start_x = x_centre(room1), start_y = up_left_y(room1), start_dir = North, end_x = up_left_x(room2), end_y = y_centre(room2), bend_x = x_centre(room1), bend_y = y_centre(room2), bend_dir = East}
 
-                                                                      else 
+                                                                      else
                                                                          PathData {start_x = down_right_x(room1), start_y = ((down_right_y(room2) + down_right_y(room1)) `div` 2), start_dir = East, end_x = x_centre(room2), end_y = down_right_y(room2), bend_x = x_centre(room2), bend_y = ((down_right_y(room2) + down_right_y(room1)) `div` 2), bend_dir = North}
 
-                           | (y_centre(room2) > y_centre(room1)) = if (up_left_y(room2) > y_centre(room1)) then 
+                           | (y_centre(room2) > y_centre(room1)) = if (up_left_y(room2) > y_centre(room1)) then
                                                                       PathData {start_x = up_left_x(room1), start_y = y_centre(room1), start_dir = East, end_x = x_centre(room2), end_y = up_left_y(room2), bend_x = x_centre(room2), bend_y = y_centre(room1), bend_dir = South}
 
-                                                                   else 
+                                                                   else
                                                                       if (down_right_y(room1) < y_centre(room2)) then
                                                                          PathData {start_x = x_centre(room1), start_y = down_right_y(room1), start_dir = South, end_x = down_right_x(room2), end_y = y_centre(room2), bend_x = x_centre(room1), bend_y = y_centre(room2), bend_dir = East}
 
-                                                                      else 
+                                                                      else
                                                                          PathData {start_x = up_left_x(room1), start_y = ((up_left_y(room2) + up_left_y(room1)) `div` 2), start_dir = East, end_x = x_centre(room2), end_y = up_left_y(room2), bend_x = x_centre(room2), bend_y = ((up_left_y(room2) + up_left_y(room1)) `div` 2), bend_dir = South}
-                                            
+
     -- Driver function to build paths between all rooms
     make_all_paths :: [RoomData] -> [PathData]
-    make_all_paths list_of_rooms = 
-                           let 
+    make_all_paths list_of_rooms =
+                           let
                            sorted_rooms = sort_all_rooms list_of_rooms
                            zipped_rooms = zip (sorted_rooms) (tail(sorted_rooms))
                            in make_all_paths_impl zipped_rooms
@@ -143,7 +144,7 @@ module Dungeon where
     make_all_paths_impl :: [(RoomData, RoomData)] -> [PathData]
     make_all_paths_impl [] = []
     make_all_paths_impl zipped_rooms = make_path(head(zipped_rooms)) : make_all_paths_impl(tail(zipped_rooms))
-    
+
     -- Functions to "make" a displayable floor object
     -- This floor object is a list of lists of Datatype Tile
 
@@ -156,21 +157,21 @@ module Dungeon where
     floor_init :: Int -> Int -> Tile -> [[Tile]]
     floor_init _ 0 _ = []
     floor_init lngth brdth tiletype = (floor_row_init lngth tiletype) : (floor_init (lngth) (brdth - 1) tiletype)
-    
+
     -- Implicit function to modify the entry of a row in a floor
     floor_row_mod :: Int -> Int -> [Tile] -> Tile -> [Tile]
     floor_row_mod start end row new_tile = (take start row) ++ (floor_row_init (end - start + 1) new_tile) ++ (drop (end + 1) row)
-    
+
     -- Implicit function to modify a set of rows in a floor
     floor_rows_mod :: Int -> Int -> [[Tile]] -> Tile -> [[Tile]]
     floor_rows_mod _ _ [] _ = []
     floor_rows_mod start end cur_floor new_tile = (floor_row_mod start end (head(cur_floor)) new_tile) : floor_rows_mod start end (tail(cur_floor)) new_tile
-    
+
     -- Driver function to "make" my floor object based on the Rooms I have
     floor_build :: [RoomData] -> [[Tile]] -> Tile -> [[Tile]]
     floor_build [] cur_floor _ = cur_floor
-    floor_build my_rooms cur_floor new_tile = floor_build (tail(my_rooms)) (floor_room_mod (head(my_rooms)) cur_floor new_tile) new_tile 
-    
+    floor_build my_rooms cur_floor new_tile = floor_build (tail(my_rooms)) (floor_room_mod (head(my_rooms)) cur_floor new_tile) new_tile
+
     -- Implicit function to modify the floor based on the parameters of one room
     floor_room_mod :: RoomData -> [[Tile]] -> Tile -> [[Tile]]
     floor_room_mod my_room cur_floor new_tile = let
@@ -182,21 +183,17 @@ module Dungeon where
                                                 end_x = down_right_x my_room
                                                 in
                                                 unchange_before ++ (floor_rows_mod start_x end_x change_floor new_tile) ++ unchange_after
-                                                
+
     -- Function to display the floor. Just for testing and validating purposes.
-    floor_display :: [[Tile]] -> IO ()
-    floor_display [] = do
-                     putStrLn ("\n")
-    floor_display my_floor = do
-                           putStrLn ((show (head(my_floor))) ++ "\n")
-                           floor_display (tail(my_floor))
-                           
-    -- Function to convert a set of paths into a set of rooms. 
+    floor_display :: [[Tile]] -> [[String]]
+    floor_display tiles = map (\a -> map show a) tiles
+
+    -- Function to convert a set of paths into a set of rooms.
     -- Paths can be considered to be a 1D room
     convert_paths_to_rooms :: [PathData] -> [RoomData]
     convert_paths_to_rooms [] = []
     convert_paths_to_rooms my_paths = convert_path_to_room (head(my_paths)) ++ convert_paths_to_rooms (tail(my_paths))
-    
+
     -- Implicit function to convert a path into a list of 1 or 2 rooms depending upon whether a bend exists or not
     convert_path_to_room :: PathData -> [RoomData]
     convert_path_to_room cur_path = if (bend_dir(cur_path) == None) then
@@ -205,29 +202,29 @@ module Dungeon where
                                        in
                                        (room1 : [])
                                     else
-                                       let 
+                                       let
                                        room1 = build_room (start_x(cur_path)) (start_y(cur_path)) (bend_x(cur_path)) (bend_y(cur_path))
                                        room2 = build_room (end_x(cur_path)) (end_y(cur_path)) (bend_x(cur_path)) (bend_y(cur_path))
                                        in
                                        (room1 : (room2 : []))
-                                       
+
     -- Driver function to embed paths into the floor
     path_build :: [PathData] -> [[Tile]] -> [[Tile]]
     path_build my_paths cur_floor = let
                                     all_paths_are_rooms = convert_paths_to_rooms my_paths
                                     in
                                     floor_build all_paths_are_rooms cur_floor Path
-                                    
+
     get_entry_point :: [RoomData] -> (Int, Int)
-    get_entry_point list_of_rooms = let 
+    get_entry_point list_of_rooms = let
                                     slor = sort_all_rooms list_of_rooms
                                     x = up_left_x(head(slor))
                                     y = down_right_y(head(slor))
                                     in
                                     (x,y)
-                                    
+
     get_exit_point :: [RoomData] -> (Int, Int)
-    get_exit_point list_of_rooms = let 
+    get_exit_point list_of_rooms = let
                                    slor = sort_all_rooms list_of_rooms
                                    x = down_right_x(last(slor))
                                    y = up_left_y(last(slor))
--- EventLoop.hs
+++ EventLoop.hs
@@ -1,13 +1,16 @@
 module EventLoop where
+    import Entity
     import Dungeon
     import System.Random
     import System.IO
     import Control.Monad
+    import Data.List
 
-    data DungeonMap = DungeonMap { dung_floor :: [[Tile]], path_data :: [PathData], entry_point :: (Int, Int), exit_point :: (Int, Int)}
-    data Inputs = Whatever | Quit | Display deriving(Eq)
+    data DungeonMap = DungeonMap { dung_floor :: [[Tile]], path_data :: [PathData], entry_point :: (Int, Int), exit_point :: (Int, Int), player :: Character, monsters :: [(Character,(Int,Int))]}
+    --data Inputs = Whatever | Quit | Display deriving(Eq)
+    data Inputs = Quit | MLeft | MDown | MUp | MRight | Quaff | Wield | Whatever
 
-    eventloop = do
+    eventloop pchar_init = do
                 let rand_n = randomRs (0, maxBound :: Int) (mkStdGen 0)
                 let (n, lor) = place_rooms 10 10 10 rand_n
                 let entry = get_entry_point lor
@@ -18,31 +21,38 @@ module EventLoop where
                 let floor_2 = floor_build lor floor_1 Room
                 let floor_3 = floor_build (exit_room:[]) floor_2 Exit
                 let floor_4 = path_build all_p floor_3
-                game_loop (DungeonMap floor_4 all_p entry exit)
-       
+                game_loop (DungeonMap floor_4 all_p entry exit pchar_init [])
+
     game_loop dung_map = do
-                         input <- get_input
-                         case input of 
-                              Quit    -> handle_exit
-                              Display -> handle_display dung_map
-                              Whatever-> handle_input dung_map input
-                                           
+                            handle_display dung_map
+                            input <- get_input
+                            case input of
+                                Quit -> handle_exit
+                                MLeft -> game_loop DungeonMap {dung_floor=dung_floor dung_map,path_data=path_data dung_map,entry_point=(-1 + (fst (entry_point dung_map)),snd (entry_point dung_map)),exit_point=exit_point dung_map,player = player dung_map,monsters=[]}
+                                MDown -> game_loop DungeonMap {dung_floor=dung_floor dung_map,path_data=path_data dung_map,entry_point=(fst (entry_point dung_map),1 + (snd (entry_point dung_map))),exit_point=exit_point dung_map,player = player dung_map,monsters=[]}
+                                MUp -> game_loop DungeonMap {dung_floor=dung_floor dung_map,path_data=path_data dung_map,entry_point=(fst (entry_point dung_map),-1 + (snd (entry_point dung_map))),exit_point=exit_point dung_map,player = player dung_map,monsters=[]}
+                                MRight -> game_loop DungeonMap {dung_floor=dung_floor dung_map,path_data=path_data dung_map,entry_point=(1 + (fst (entry_point dung_map)),snd (entry_point dung_map)),exit_point=exit_point dung_map,player = player dung_map,monsters=[]}
+                                Quaff -> game_loop dung_map
+                                Wield -> game_loop dung_map
+                                Whatever -> game_loop dung_map
+
     -- to be modified as per your requirements
     get_input = do
-                what_to_do <- getChar 
+                what_to_do <- getChar
                 case what_to_do of
-                     '0' -> return Quit
-                     '1' -> return Display
-                     _   -> return Whatever                     
-                
-    handle_input dung_map input = do
-                                  putStrLn "DO SOMETHING!!\n"
-                                  game_loop dung_map
+                     'x' -> return Quit
+                     'h' -> return MLeft
+                     'j' -> return MDown
+                     'k' -> return MUp
+                     'l' -> return MRight
+                     'q' -> return Quaff
+                     'w' -> return Wield
+                     _   -> return Whatever
 
     handle_display dung_map = do
-                              floor_display (dung_floor(dung_map))
-                              game_loop dung_map
-                              
+                            
+                            putStrLn (intercalate "\n" (map (\a -> intercalate "" a) (floor_display (dung_floor(dung_map)))))
+
     handle_exit = do
                   putStrLn "GGWP!!\n"
                   return ()
